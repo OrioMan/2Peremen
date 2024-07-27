aca371bfa230e75fc47b028a5d5f7e1e41525703
OrioMan <sielsan1@gmail.com>
2024-07-27 12:42:27 +0300

    Add files via upload

---
+++ 2Peremen.py
@@ -0,0 +1,5 @@
+homework_count = 12
+time_spent = 1.5
+сourse_name = 'Python'
+time_for_one_task = time_spent / homework_count
+print('Курс:', сourse_name, ', всего задач:', homework_count,' , затрачено часов:', time_spent,', cреднее время выполнения',time_for_one_task,'часа.')
